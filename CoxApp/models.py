3ffe015df51252bf40b9792b327947a18524a60c
cox <gmhesat@gmail.com>
2018-12-20 18:13:37 +0800

    Update models.py

--- CoxApp/models.py
+++ CoxApp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+"""
+1. 学院
+"""
+class Academy(models.Model):
+    name = models.CharField(verbose_name="学院名",max_length=64)
+    department = models.OneToOneField("Department",on_delete=models.CASCADE,verbose_name="系")
+
+class Department(models.Model):
+    name = models.CharField(verbose_name="系名",max_length=64)
